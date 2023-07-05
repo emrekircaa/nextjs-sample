b007027bffb9df62eb538a319613cf0de1634b7a
Emre KIRCA <79770308+emrekircaa@users.noreply.github.com>
2023-07-05 19:56:34 +0300

    Update InfoCard.tsx

--- components/InfoCard.tsx
+++ components/InfoCard.tsx
@@ -6,13 +6,19 @@ function InfoCard(props: {
   revenue: string;
   color: string;
 }) {
+  console.log(props.name);
   return (
     <div
       className="max-w-96 max-h-[156px] rounded-lg p-5"
       style={{ backgroundColor: props.color }}
     >
-      <div className="w-12 h-9 mb-4">
-        <Image src={props.path.src} height={32} width={38} alt={props.name} />
+      <div className=" h-9 mb-4">
+        <Image
+          src={props.path.src}
+          height={32}
+          width={props.name == "Student" ? 55 : 38}
+          alt={props.name}
+        />
       </div>
       <span className="font-medium text-gray text-sm">{props.name}</span>
       <span className="flex justify-end items-end text-xlarge font-bold text-black ">
